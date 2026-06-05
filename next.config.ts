ae2e3a821725f2ad9ef4ab2391a48c60c9fa192b
Khushi Kumari <khushikumari240824@gmail.com>
2026-06-05 20:41:12 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-import { fileURLToPath } from "url";
-
-const workspaceRoot = fileURLToPath(new URL(".", import.meta.url));
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: workspaceRoot,
-  },
 };
 
 export default nextConfig;
