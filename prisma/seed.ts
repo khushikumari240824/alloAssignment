dcb54cd28f77db664fd4b7355735ab37c573f5c6
Khushi Kumari <khushikumari240824@gmail.com>
2026-06-05 19:10:04 +0530

    commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import "dotenv/config";
+import { prisma } from "../src/lib/db";
 
 async function main() {
   console.log("Cleaning up database...");
